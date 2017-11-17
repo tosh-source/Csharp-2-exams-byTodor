1b31cb43ba5e3a9f319f87ede0d07336cd6c6c54
user <user@USER-VIRTUAL_PC>
2017-11-17 18:39:37 +0200

    Changes: 1.Change the algorithm of program. Now the program can catch pair of tags at any line. I.e., the text will be read as one and be processed.  The old version of program read text line by line. 2. Speed up in "ManipulateTextMethod" in strings.

--- 11.Feb.2013-Task4.FakeTextMarkupLanguage/Program.cs
+++ 11.Feb.2013-Task4.FakeTextMarkupLanguage/Program.cs
@@ -14,42 +14,39 @@ namespace _11.Feb._2013_Task4.FakeTextMarkupLanguage
         { //condition & BGCoder: http://bgcoder.com/Contests/55/CSharp-Part-2-2012-2013-11-Feb-2013
 
             //tests
-            var testInput = new StringReader(@"3
-
-<toggle><rev>ERa</rev></toggle> you
-<rev>noc</rev><lower>FUSED</lower");
-            Console.SetIn(testInput);
+//            var testInput = new StringReader(@"3
+//<toggle><rev>ERa</rev></toggle> you
+//<rev>noc</rev><lower>FUSED</lower>
+//<rev>?<rev>already </rev></rev>");
+//            Console.SetIn(testInput);
 
             //input
             short numberOfLines = short.Parse(Console.ReadLine());
-            string[] text = new string[numberOfLines];
+            string text = string.Empty;
+            StringBuilder textAsSB = new StringBuilder();
+
             for (short line = 0; line < numberOfLines; line++)
             {
-                text[line] = Console.ReadLine().ToString();
+                textAsSB.Append(Console.ReadLine() + "\n");  //"Environment.NewLine" is better choice, "\n" is only for BGCoder
             }
+            text = textAsSB.ToString();
 
             //calculation
             int currentCloseIndex = -1;
             int deepestOpenIndex = -1;
             int deepestCloseIndex = 0;
             int deepestTag = 0;
-            StringBuilder currentLine = new StringBuilder();
             StringBuilder subText = new StringBuilder();
 
-            Calculations(ref text, ref currentCloseIndex, ref deepestCloseIndex, ref deepestOpenIndex, ref deepestTag, currentLine, subText);
+            Calculations(textAsSB, ref text, ref currentCloseIndex, ref deepestCloseIndex, ref deepestOpenIndex, ref deepestTag, subText);
 
             //print
-            Console.WriteLine(string.Join("\n", text));
+            Console.WriteLine(text);
         }
 
-        private static void Calculations(ref string[] text, ref int currentCloseIndex, ref int deepestCloseIndex, ref int deepestOpenIndex, ref int deepestTag, StringBuilder currentLine, StringBuilder subText)
+        private static void Calculations(StringBuilder textAsSB, ref string text, ref int currentCloseIndex, ref int deepestCloseIndex, ref int deepestOpenIndex, ref int deepestTag, StringBuilder subText)
         {
-            for (short line = 0; line < text.Length; line++)
-            {
-                currentLine.Clear();
-                currentLine.Append(text[line]);
-
-                for (int processingLine = 0; processingLine < text[line].Length; processingLine++)
+                for (int processingSymbols = 0; processingSymbols < text.Length; processingSymbols++)
                 {
                     bool continueOrNot = false;
 
@@ -58,7 +55,7 @@ namespace _11.Feb._2013_Task4.FakeTextMarkupLanguage
 
                     for (byte current = 0; current < closedTags.Length; current++)
                     {
-                        currentCloseIndex = text[line].IndexOf(closedTags[current], 0);
+                        currentCloseIndex = text.IndexOf(closedTags[current], 0);
 
                         if ((deepestCloseIndex == 0 || currentCloseIndex <= deepestCloseIndex) && currentCloseIndex != -1)    //condition: "deepestCloseIndex == 0"                 , will parse FIRST founded "close tag"
                         {                                                                                                     //condition: "currentCloseIndex <= deepestCloseIndex" , will parse any other DEEPER "close tag"
@@ -71,43 +68,42 @@ namespace _11.Feb._2013_Task4.FakeTextMarkupLanguage
                     if (continueOrNot == false) break; //stop processing if no more tags and step to the next line
 
                     //find deepest "opened tag"
-                    deepestOpenIndex = text[line].LastIndexOf(openedTags[deepestTag], deepestCloseIndex);
+                    deepestOpenIndex = text.LastIndexOf(openedTags[deepestTag], deepestCloseIndex);
 
                     //manipulate text
-                    currentLine.Remove(deepestOpenIndex, (deepestCloseIndex - deepestOpenIndex) + closedTags[deepestTag].Length);
-                    currentLine.Insert(deepestOpenIndex,
-                                       ManipulateTextMethod(subText, ref text, line, deepestTag, deepestOpenIndex, deepestCloseIndex));
+                    textAsSB.Remove(deepestOpenIndex, (deepestCloseIndex - deepestOpenIndex) + closedTags[deepestTag].Length);
+                    textAsSB.Insert(deepestOpenIndex,
+                                       ManipulateTextMethod(subText, ref text, deepestTag, deepestOpenIndex, deepestCloseIndex));
 
-                    text[line] = currentLine.ToString();
+                    text = textAsSB.ToString();
                 }
-            }
         }
 
-        private static StringBuilder ManipulateTextMethod(StringBuilder subText, ref string[] text, int line, int deepestTag, int deepestOpenIndex, int deepestCloseIndex)
+        private static StringBuilder ManipulateTextMethod(StringBuilder subText, ref string text, int deepestTag, int deepestOpenIndex, int deepestCloseIndex)
         {
             subText.Clear();
 
             switch (deepestTag)
             {
                 case 0: //The <upper> tag converts text to its uppercase variant
-                    subText.Append(text[line]
-                                   .Substring(deepestOpenIndex + "<upper>".Length, deepestCloseIndex - (deepestOpenIndex + "<upper>".Length))
-                                   .ToUpper());
+                    subText.Append(text.Substring(deepestOpenIndex + openedTags[deepestTag].Length, 
+                                                  deepestCloseIndex - (deepestOpenIndex + openedTags[deepestTag].Length))
+                                                 .ToUpper());
                     break;
                 case 1: //The <lower> tag converts text to its lowercase variant
-                    subText.Append(text[line]
-                                   .Substring(deepestOpenIndex + "<lower>".Length, deepestCloseIndex - (deepestOpenIndex + "<lower>".Length))
-                                   .ToLower());
+                    subText.Append(text.Substring(deepestOpenIndex + openedTags[deepestTag].Length, 
+                                                  deepestCloseIndex - (deepestOpenIndex + openedTags[deepestTag].Length))
+                                                 .ToLower());
                     break;
                 case 2: //The <rev> tag reverses all text in it
-                    subText.Append(text[line]
-                                   .Substring(deepestOpenIndex + "<rev>".Length, deepestCloseIndex - (deepestOpenIndex + "<rev>".Length))
-                                   .Reverse().ToArray());
+                    subText.Append(text.Substring(deepestOpenIndex + openedTags[deepestTag].Length, 
+                                                  deepestCloseIndex - (deepestOpenIndex + openedTags[deepestTag].Length))
+                                                 .Reverse().ToArray());
                     break;
                 case 3: //"<toggle>" tag rules -> if a character is uppercase, it converts it to lowercase
                         //                        if a character is lowercase, it converts it to uppercase
-                    string temp = text[line]
-                                  .Substring(deepestOpenIndex + "<toggle>".Length, deepestCloseIndex - (deepestOpenIndex + "<toggle>".Length));
+                    string temp = text.Substring(deepestOpenIndex + openedTags[deepestTag].Length, 
+                                                 deepestCloseIndex - (deepestOpenIndex + openedTags[deepestTag].Length));
 
                     for (int i = 0; i < temp.Length; i++)
                     {
